17c9244f9f670c9abd38ef1856e9bc1958b82c54
andresestupinan14 <andresestupinancolpre2018@gmail.com>
2025-09-10 09:39:52 -0500

    Add files via upload

--- Corte1/C++/Practica1/main.cpp
+++ Corte1/C++/Practica1/main.cpp
@@ -1,1 +1,11 @@
+#include <iostream>
 
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	
+	
+	std::cout << "Bienvenidos a C++" << std::endl;
+	
+	return 0;
+}
